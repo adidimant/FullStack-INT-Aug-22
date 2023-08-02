5cc60a10bb0b26a974a479e46b48456d7a9a1130
adidimant <adi.dimant@transmitsecurity.com>
2023-08-02 17:39:15 +0300

    clean main App file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-/* set PUBLIC_URL=login&&*/
-
 import { PostsProvider } from "./postsContext/PostConext";
 import Posts from "./components/Posts";
-import GreenLight from "./greenLight/GreenLight";
 import TopPosts from "./components/TopPosts";
 import CreatePost from "./components/CreatePost";
-import CreatePromise from "./promise/CreatePromise";
 import Login from "./components/Login";
 import { Navigate } from "react-router-dom";
 
@@ -19,14 +14,12 @@ export default function App() {
       <PostsProvider>
         <Routes>
           <Route path="/getPosts" element={<Posts />} />
-          {/* <Route path="/topPosts" element={<TopPosts />} /> */}
+          <Route path="/topPosts" element={<TopPosts />} />
           <Route path="/upload-post" element={<CreatePost />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </PostsProvider>
-      {/* <GreenLight /> */}
-      {/* <CreatePromise /> */}
     </>
   );
 }
